ac2a8ed38277a897592c34750a782e8e42032ff0
Jayden <jayden.rerimassie@gmail.com>
2025-12-03 17:05:21 +0100

    Klant model aangemaakt in Class Library

---
+++ CL/Models/Klant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL.Models
+{
+    public class Klant
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; } = "";
+        public string Tussenvoegsel { get; set; } = "";
+        public string Achternaam { get; set; } = "";
+        public string Email{ get; set; } = "";
+        public string Telefoonnummer { get; set; } = "";
+        public DateTime RegistratieDatum {  get; set; } = DateTime.Now;
+    }
+}
